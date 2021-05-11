fc5130d5c3e6d6adca4e71fc7955ec4cbf11e770
hayatullah farahi <hayatullah.farahi@gmail.com>
2021-05-11 09:11:43 +0430

    style changes

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class Utils {
-  final Color primaryColor = Color(0xff48C4A4);
+  final Color primaryColor = goldColor;
+//  final Color primaryColor = Color(0xff48C4A4);
 //  final Color primaryColor = Color(0xff2bbbad);
-  final baseUrl = "https://192.168.1.8:5001";
+  final baseUrl = "https://192.168.1.128:5001";
   final localHost = Platform.isAndroid ? "http://10.0.2.2:5001" : "http://localhost:5001";
   //http://192.168.5.22:8088/ ATN network m16status hosted
 }
+
+final Color blueColor = Color(0xff005a84);
+final Color goldColor = Color(0xffcbbe95);
--- lib/widgets/search_form.dart
+++ lib/widgets/search_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:m16status/models/search_model.dart';
 import 'package:m16status/screens/m16_details_screen.dart';
 import 'package:m16status/state/app_state.dart';
@@ -32,126 +33,123 @@ class _SearchFromState extends State<SearchFrom> {
         key: _m16SearchFormKey,
         child: Container (
             width: MediaQuery.of (context).size.width,
-            child: Column (
-                children: <Widget>[
-                  Consumer<AppState>(
-                    builder: (context, state, child) {
-                      return FutureBuilder(
-                          future: state.getSearchFormData(),
-                          builder: (BuildContext context, AsyncSnapshot snapshot) {
-                            if( snapshot.data == null )
-                              return Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(18.0),
+            child: Consumer<AppState>(
+              builder: (context, state, child) {
+                return FutureBuilder(
+                    future: state.getSearchFormData(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if( snapshot.data == null )
+                        return Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(18.0),
+//                            child: SpinKitRing(color: Theme.of(context).primaryColor),
                                   child: CircularProgressIndicator(
                                     backgroundColor: Theme.of(context).primaryColor,
                                   ),
-                                )
-                              );
-                            else {
-                              SearchModel searchModel = snapshot.data;
-                              return Column(
-                                children: [
-                                  //put Custom drop down here based on snapshot data
-                                  InputContainer(
-                                    height: null,
-                                    child: SearchableDropdown(
-                                      items: searchModel.organizationsDescriptions.map((item) {
-                                        return DropdownMenuItem<OrganizationsDescription>(
-                                            child: Text("${item.organizationDescription} - (${item.code})"),
-                                            value: item);
-                                      }).toList(),
-                                      isExpanded: true,
-                                      value: _selectedOrganization,
-                                      hint: "Select organization",
-                                      searchHint: Text(
-                                        'Select organization',
-                                        style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
-                                      ),
-                                      onChanged: (value) {
-                                        setState(() {
-                                          _selectedOrganization = value;
-                                        });
-                                      },
-                                    )
+                          )
+                        );
+                      else {
+                        SearchModel searchModel = snapshot.data;
+                        return Column(
+                          children: [
+                            //put Custom drop down here based on snapshot data
+                            InputContainer(
+                              height: null,
+                              child: SearchableDropdown(
+                                items: searchModel.organizationsDescriptions.map((item) {
+                                  return DropdownMenuItem<OrganizationsDescription>(
+                                      child: Text("${item.organizationDescription} - (${item.code})"),
+                                      value: item);
+                                }).toList(),
+                                isExpanded: true,
+                                value: _selectedOrganization,
+                                hint: "Select organization",
+                                searchHint: Text(
+                                  'Select organization',
+                                  style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
                                 ),
-                                  InputContainer(
-                                      height: null,
-                                      child: SearchableDropdown(
-                                        items: searchModel.locationDescriptions.map((item) {
-                                          return DropdownMenuItem<LocationDescription>(
-                                              child: Text("${item.description} - (${item.provinceCode})"),
-                                              value: item);
-                                        }).toList(),
-                                        isExpanded: true,
-                                        value: _selectedLocation,
-                                        hint: "Select location",
-                                        searchHint: Text(
-                                          'Select location',
-                                          style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
-                                        ),
-                                        onChanged: (value) {
-                                          setState(() {
-                                            _selectedLocation = value;
-                                          });
-                                        },
-                                      )
+                                onChanged: (value) {
+                                  setState(() {
+                                    _selectedOrganization = value;
+                                  });
+                                },
+                              )
+                          ),
+                            InputContainer(
+                                height: null,
+                                child: SearchableDropdown(
+                                  items: searchModel.locationDescriptions.map((item) {
+                                    return DropdownMenuItem<LocationDescription>(
+                                        child: Text("${item.description} - (${item.provinceCode})"),
+                                        value: item);
+                                  }).toList(),
+                                  isExpanded: true,
+                                  value: _selectedLocation,
+                                  hint: "Select location",
+                                  searchHint: Text(
+                                    'Select location',
+                                    style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
                                   ),
-                                  InputContainer(
-                                      height: null,
-                                      child: SearchableDropdown(
-                                        items: searchModel.budgetTypes.map((item) {
-                                          return DropdownMenuItem<BudgetType>(
-                                              child: Text("${item.description} - (${int.parse(item.code) + 1})"),
-                                              value: item);
-                                        }).toList(),
-                                        isExpanded: true,
-                                        value: _selectedBudgetType,
-                                        hint: "Select budget type",
-                                        searchHint: Text(
-                                          'Select budget type',
-                                          style: TextStyle(fontSize: 20),
-                                        ),
-                                        onChanged: (value) {
-                                          setState(() {
-                                            _selectedBudgetType = value;
-                                          });
-                                        },
-                                      )
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedLocation = value;
+                                    });
+                                  },
+                                )
+                            ),
+                            InputContainer(
+                                height: null,
+                                child: SearchableDropdown(
+                                  items: searchModel.budgetTypes.map((item) {
+                                    return DropdownMenuItem<BudgetType>(
+                                        child: Text("${item.description} - (${int.parse(item.code) + 1})"),
+                                        value: item);
+                                  }).toList(),
+                                  isExpanded: true,
+                                  value: _selectedBudgetType,
+                                  hint: "Select budget type",
+                                  searchHint: Text(
+                                    'Select budget type',
+                                    style: TextStyle(fontSize: 20),
                                   ),
-                                  InputContainer(
-                                      height: 45,
-                                      child: TextFormField(
-                                          keyboardType: TextInputType.number,
-                                          controller: _m16Controller,
-                                          decoration: InputDecoration(
-                                              border: InputBorder.none,
-                                              hintText: 'M16 Code'
-                                          )
-                                      )
-                                  ),
-                                ],
-                              );
-                            }
-                          }
-                      );
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedBudgetType = value;
+                                    });
+                                  },
+                                )
+                            ),
+                            InputContainer(
+                                height: 45,
+                                child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    controller: _m16Controller,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'M16 Code'
+                                    )
+                                )
+                            ),
+                            SizedBox(height: 25),
+                            // ignore: deprecated_member_use
+                            RaisedButton.icon(
+                              onPressed: _submitForm,
+                              color: Theme.of(context).primaryColor,
+                              label: Text(
+                                  "Search M16",
+                                  style: TextStyle (
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  )
+                              ),
+                              icon: Icon(Icons.search, color: Colors.white),
+                            ),
+                          ],
+                        );
+                      }
                     }
-                  ),
-                  SizedBox(height: 25),
-                    // ignore: deprecated_member_use
-                  RaisedButton.icon(
-                        onPressed: _submitForm,
-                        color: Theme.of(context).primaryColor,
-                        label: Text(
-                            "Search M16",
-                            style: TextStyle (
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold
-                            )
-                        ),
-                        icon: Icon(Icons.search, color: Colors.white),
-                    ),
-                ]
+                );
+              }
             )
         ),
       )
@@ -164,7 +162,7 @@ class _SearchFromState extends State<SearchFrom> {
     ) {
       MsgDialog.showMsgDialog(context, 'Alert!', "Please fill all the fields");
     } else {
-      LoadingDialog.showLoadingDialog(context, 'Search...');
+      LoadingDialog.showLoadingDialog(context, 'Searching...');
       final _appState = Provider.of<AppState>(context, listen: false);
       Map<String, dynamic> successInfo = await _appState.getM16Details({
         "m16": _m16Controller.text,
@@ -176,6 +174,12 @@ class _SearchFromState extends State<SearchFrom> {
       });
       if (successInfo['success'] && successInfo['stageCount'] > 0) {
         LoadingDialog.hideLoadingDialog(context);
+        setState(() {
+          _m16Controller.text = "";
+          _selectedBudgetType = null;
+          _selectedLocation = null;
+          _selectedOrganization = null;
+        });
         Navigator.of(context).push(
             MaterialPageRoute(
                 builder: (BuildContext context) =>
